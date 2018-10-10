f49479348ff4a11ddac305b3ad1be5d21f9c2580
Sankalp100 <sankalpgour123@gmail.com>
2018-10-10 21:51:41 +0530

    fifth commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var origBoard;
-const huPlayer = '0';
+const huPlayer = 'o';
 const aiPlayer = 'X';
 const winCombos = [
     [0,1,2],
@@ -17,7 +17,7 @@ startGame();
 
 function startGame() {
     document.querySelector(".endgame").style.display = "none";
-    origBoard Array.from(Array(9).keys());
+    origBoard = Array.from(Array(9).keys());
     for(var i=0; i < cells.length; i++){
         cells[i].innerText = '';
         cells[i].style.removeProperty('background-color');
@@ -26,5 +26,52 @@ function startGame() {
 }    
 
 function turnClick(square){
-    turn(square.target.id, huPlayer);
-}    
+    turn(square.target.id, huPlayer)
+}
+
+function turn(squareId, player){
+    origBoard[squareId] = player;
+    document.getElementById(squareId).innerText = player;
+    let gameWon = checkWin(origBoard, player)
+    if (gameWon) gameOver(gameWon)
+}
+
+function checkWin(board, player){
+    let plays = board.reduce((a, e, i) => 
+     (e === player) ? a.concat(i) : a, []);
+    let gameWon = null;
+    for(let [index, win] of winCombos.entries()){
+        if(win.every(elem => plays.indexOf(elem > -1)){
+            gameWon = {index: index, player: player};
+        })
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
